dd332c3981818d780a7184093667616df21a875f
Fienny Angelina <fifi.angelina@yahoo.com>
2018-05-30 15:18:24 +0800

    Add fetch from github Graphql by using apollo customer's client I dont use HOC like <Query> or graphql since the query is dynamic depending on the number of tag (since different tag needs to be inside differen query). Also added my fetchMore implementation which will fetch more when user click button next and their distance to the last entry is below certain threshold
    Bug #18

--- .neutrinorc.js
+++ .neutrinorc.js
@@ -5,13 +5,22 @@ module.exports = {
         title: 'Codetribute'
       }
     }],
+    ['@neutrinojs/env', ['GITHUB_PERSONAL_ACCESS_TOKEN']],
     (neutrino) => {
-      neutrino.config.output.publicPath('/');
-      neutrino.config.module
-        .rule('js-yaml')
-        .test(/\.(yaml|yml)$/)
-        .use('js-yaml-loader')
-        .loader('js-yaml-loader');
-    }
+    neutrino.config.output.publicPath('/');
+    neutrino.config.module
+      .rule('js-yaml')
+      .test(/\.(yaml|yml)$/)
+      .use('js-yaml-loader')
+      .loader('js-yaml-loader');
+    neutrino.config.module
+      .rule('graphql')
+      .test(/\.graphql$/)
+      .include
+      .add(neutrino.options.source)
+      .end()
+      .use('gql-loader')
+      .loader(require.resolve('graphql-tag/loader'));
+  }
   ],
 };
