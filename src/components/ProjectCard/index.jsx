173c5230b9fa771e06c6614af514dbd49050d16f
Fienny Angelina <24544912+fiennyangeln@users.noreply.github.com>
2018-05-31 00:12:41 +0800

    Create homepage
    - Add routing to the homepage
    - Change material-ui version

---
+++ src/components/ProjectCard/index.jsx
@@ -0,0 +1,77 @@
+import {
+  withStyles,
+  Card,
+  CardContent,
+  CardActions,
+  Typography,
+} from 'material-ui';
+import Button from 'material-ui/Button';
+import { Component } from 'react';
+import { string } from 'prop-types';
+import ReactMarkdown from 'react-markdown';
+
+@withStyles(theme => ({
+  card: {
+    textAlign: 'center',
+    position: 'relative',
+    width: '100%',
+    boxShadow: '0 1px 4px 0 rgba(0, 0, 0, 0.14)',
+    minHeight: 250,
+  },
+  textAlign: {
+    textAlign: 'center',
+  },
+  cardSubtitle: {
+    color: '#999999',
+    fontWeight: 300,
+    lineHeight: '1.5em',
+    fontSize: '1em',
+    textTransform: 'uppercase',
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  cardDescription: {
+    fontWeight: 300,
+    padding: 2 * theme.spacing.unit,
+  },
+  cardActions: {
+    position: 'absolute',
+    right: 0,
+    left: 0,
+    bottom: 0,
+    display: 'flex',
+    justifyContent: 'center',
+  },
+}))
+export default class ProjectCard extends Component {
+  static propTypes = {
+    title: string.isRequired,
+    description: string,
+  };
+
+  static defaultProps = {
+    description: null,
+  };
+
+  render() {
+    const { classes, title, description } = this.props;
+
+    return (
+      <Card className={classes.card}>
+        <CardContent className={classes.textAlign}>
+          <Typography gutterBottom variant="title" component="h4">
+            {title}
+          </Typography>
+          {description && (
+            <Typography className={classes.cardDescription}>
+              <ReactMarkdown source={description} />
+            </Typography>
+          )}
+        </CardContent>
+        <CardActions className={classes.cardActions}>
+          <Button color="primary">VIEW PROJECT</Button>
+        </CardActions>
+      </Card>
+    );
+  }
+}
---
+++ src/views/Projects/index.jsx
@@ -0,0 +1,51 @@
+import { Component, Fragment } from 'react';
+import { withStyles } from 'material-ui/styles';
+import Grid from 'material-ui/Grid';
+import Typography from 'material-ui/Typography';
+import projects from '../../data/loader';
+import ProjectCard from '../../components/ProjectCard';
+
+@withStyles(theme => ({
+  container: {
+    paddingRight: 15,
+    paddingLeft: 15,
+    marginRight: 'auto',
+    marginLeft: 'auto',
+  },
+  header: {
+    paddingTop: theme.spacing.unit,
+    paddingBottom: 2 * theme.spacing.unit,
+  },
+}))
+export default class Projects extends Component {
+  state = {
+    projects,
+  };
+
+  render() {
+    const { classes } = this.props;
+    const { projects } = this.state;
+
+    return (
+      <Fragment>
+        <header className={classes.header}>
+          <Typography variant="display2" align="center">
+            Bug Issue
+          </Typography>
+          <Typography variant="subheading" align="center">
+            Find your first contribution with Mozilla
+          </Typography>
+        </header>
+        <main className={classes.container}>
+          <Grid container spacing={16}>
+            {Object.entries(projects).map(([project, info]) => (
+              <Grid item key={project} xs={12} sm={12} md={4} lg={3}>
+                <ProjectCard title={info.name} description={info.description} />
+              </Grid>
+            ))}
+          </Grid>
+        </main>
+      </Fragment>
+    );
+  }
+}
