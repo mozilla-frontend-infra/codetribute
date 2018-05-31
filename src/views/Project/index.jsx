7eb1537904f01ca40c37cb1d01c0a504b433a575
Fienny Angelina <fifi.angelina@yahoo.com>
2018-05-31 17:10:46 +0800

    Move client.query from table components to view

--- src/views/Project/index.jsx
+++ src/views/Project/index.jsx
@@ -1,10 +1,14 @@
 import { hot } from 'react-hot-loader';
 import { Component, Fragment } from 'react';
 import Typography from '@material-ui/core/Typography';
-import { Component } from 'react';
+import Grid from 'material-ui/Grid';
+import { ApolloConsumer } from 'react-apollo';
+import _ from 'lodash';
+import gql from 'graphql-tag';
 import { ApolloConsumer } from 'react-apollo';
 import projects from '../../data/loader';
 import TasksTable from '../../components/TasksTable';
+import Issues from './issues.graphql';
 
 const data = [
   {
@@ -105,7 +109,7 @@ const data = [
 ];
 
 @hot(module)
-export default class Project extends Component {
+class Project extends Component {
   render() {
     const project = projects[this.props.match.params.project];
 
@@ -125,3 +129,10 @@ export default class Project extends Component {
   }
 }
 
+const ProjectClient = props => (
+  <ApolloConsumer>
+    {client => <Project client={client} match={props.match} />}
+  </ApolloConsumer>
+);
+
+export default ProjectClient;
