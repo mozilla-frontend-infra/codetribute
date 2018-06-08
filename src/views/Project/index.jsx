8d5bd36367941107b7fd32d65c7a26f2fd971763
Fienny Angelina <fifi.angelina@yahoo.com>
2018-06-08 16:58:21 +0800

    Change query to be using @graphql decorator
    Use initial query based on project parameter to differentiate the
    cache key

--- src/views/Project/index.jsx
+++ src/views/Project/index.jsx
@@ -1,124 +1,111 @@
 import { hot } from 'react-hot-loader';
 import { Component, Fragment } from 'react';
-import Typography from '@material-ui/core/Typography';
-import { Query } from 'react-apollo';
-import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
+import dotProp from 'dot-prop-immutable';
+import { mergeAll } from 'ramda';
 import uniqBy from 'lodash.uniqby';
-import { withRouter } from 'react-router-dom';
+import Typography from '@material-ui/core/Typography';
 import projects from '../../data/loader';
 import Spinner from '../../components/Spinner';
 import ErrorPanel from '../../components/ErrorPanel';
 import TasksTable from '../../components/TasksTable';
 import issuesQuery from './issues.graphql';
 
-const getProject = fileName => projects[fileName] || {};
-/*
-Example input:
-[ {"mozilla/butter": "mentored"},
-  {"mozilla/memchaser: "mentored"},
-  {"mozilla/coversheet":"good-first-bug"}
-]
-Output:
-{ "mozilla/butter": "mentored",
-  "mozilla/memchaser: "mentored",
-  "mozilla/coversheet":"good-first-bug"
-}
- */
-const mergeListOfObjects = objects =>
-  objects ? objects.reduce((prev, cur) => ({ ...prev, ...cur }), {}) : {};
-/*
-Example input:
-{ "mozilla/butter": "mentored",
-  "mozilla/memchaser: "mentored",
-  "mozilla/coversheet":"good-first-bug"
-}
-Output:
-{
-  "mentored" : ["mozilla/butter", "mozilla/memchaser"],
-  "good-first-bug" : ["mozilla/coversheet"]
-}
- */
-const reversePropertyValue = object =>
-  Object.keys(object).reduce((prev, key) => {
-    const curr = [...(prev[object[key]] || []), key];
+const tagReposMapping = repositories =>
+  Object.keys(repositories).reduce((prev, key) => {
+    const curr = [...(prev[repositories[key]] || []), key];
 
     return {
       ...prev,
-      [object[key]]: curr,
+      [repositories[key]]: curr,
     };
   }, {});
-const githubQuery = fileName => {
-  const project = getProject(fileName);
-  const repositories = mergeListOfObjects(project.repositories);
-  // Group the project based on tag / label
-  const tagReposMapping = reversePropertyValue(repositories);
-  let queryString = '';
-  const repoStateString = 'state:open';
 
-  Object.entries(tagReposMapping).forEach(([tag, repos], idx) => {
-    const repoString = repos.map(repo => `repo:${repo}`).join(' ');
-    const tagString = `label:${tag}`;
-    const queryVariables = [repoString, tagString, repoStateString].join(' ');
-    const query = `_${idx}: search(first:100, type:ISSUE, query:"${queryVariables}")\n
-      {
-      ...Issues
-      }\n`;
-
-    queryString = queryString.concat(query);
-  });
-
-  if (queryString.length === 0) {
-    return;
-  }
-
-  return queryString;
-};
-
-@withRouter
 @hot(module)
-class Project extends Component {
+@graphql(issuesQuery, {
+  options: props => ({
+    variables: {
+      searchQuery: `${props.match.params.project.repeat(10)}`,
+    },
+  }),
+})
+export default class Project extends Component {
   state = {
-    data: [],
+    loading: true,
   };
 
-  static getDerivedStateFromProps(nextProps, prevState) {
-    if (!nextProps.githubData) {
-      return;
-    }
-
-    const issuesData = Object.values(nextProps.githubData.data).reduce(
-      (previous, repoData) => [
-        ...previous,
-        ...repoData.nodes.map(issue => {
-          const obj = {
-            project: issue.repository.name,
-            id: issue.number,
-            summary: `${issue.number} - ${issue.title}`,
-            tag: issue.labels.nodes.map(node => node.name).join(','),
-            lastUpdated: issue.updatedAt,
-            assignee: issue.assignees.nodes[0]
-              ? issue.assignees.nodes[0].login
-              : 'None',
-            url: issue.url,
-          };
-
-          return obj;
-        }),
-      ],
-      []
-    );
-
-    return {
-      data: uniqBy([...prevState.data, ...issuesData], 'summary').sort(
-        (a, b) => (a.lastupdate > b.lastupdate ? -1 : 1)
-      ),
-    };
+  componentDidMount() {
+    this.load();
   }
 
-  render() {
+  fetch = searchQuery => {
+    const {
+      data: { fetchMore },
+    } = this.props;
+
+    return fetchMore({
+      query: issuesQuery,
+      variables: {
+        searchQuery,
+      },
+      updateQuery(previousResult, { fetchMoreResult }) {
+        const moreNodes = fetchMoreResult.search.nodes;
+
+        if (!moreNodes.length) {
+          return previousResult;
+        }
+
+        return dotProp.set(
+          previousResult,
+          'search.nodes',
+          moreNodes.concat(previousResult.search.nodes)
+        );
+      },
+    });
+  };
+
+  load = async () => {
     const project = projects[this.props.match.params.project];
-    const { data } = this.state;
-    const { loading, error } = this.props.githubData;
+    const repositories = mergeAll(project.repositories);
+    const tagsMapping = tagReposMapping(repositories);
+
+    await Promise.all(
+      Object.entries(tagsMapping).map(([tag, repos]) => {
+        const searchQuery = [
+          repos.map(repo => `repo:${repo}`).join(' '),
+          `label:${tag}`,
+          'state:open',
+        ].join(' ');
+
+        return this.fetch(searchQuery);
+      })
+    );
+
+    this.setState({ loading: false });
+  };
+
+  render() {
+    const {
+      data: { error, search },
+    } = this.props;
+    const { loading } = this.state;
+    const project = projects[this.props.match.params.project];
+    const issues =
+      search &&
+      uniqBy(
+        search.nodes.map(issue => ({
+          project: issue.repository.name,
+          id: issue.number,
+          summary: `${issue.number} - ${issue.title}`,
+          tag: issue.labels.nodes.map(node => node.name).join(','),
+          lastUpdated: issue.updatedAt,
+          assignee: issue.assignees.nodes[0]
+            ? issue.assignees.nodes[0].login
+            : '-',
+          url: issue.url,
+        })),
+        'summary'
+      );
 
     return (
       <Fragment>
@@ -129,21 +116,11 @@ class Project extends Component {
           <Typography variant="subheading" align="center">
             Bugs & Issues
           </Typography>
+          {error && <ErrorPanel error={error} />}
+          {loading && <Spinner />}
+          {!error && !loading && <TasksTable items={issues} />}
         </header>
-        {error && <ErrorPanel error={error} />}
-        {loading && <Spinner />}
-        {!error && !loading && <TasksTable items={data} />}
       </Fragment>
     );
   }
 }
-
-const ProjectClient = props => (
-  <Query
-    query={gql`{${githubQuery(props.match.params.project)}}\n${issuesQuery}`}
-    skip={!projects[props.match.params.project].repositories}>
-    {githubData => <Project githubData={githubData} />}
-  </Query>
-);
-
-export default ProjectClient;
