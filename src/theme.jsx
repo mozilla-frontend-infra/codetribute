2020d482a493f84f9597893116119f7d79cdecff
Fienny Angelina <24544912+fiennyangeln@users.noreply.github.com>
2018-06-18 15:01:48 -0700

    Use Roboto Font (#43)
    * Use Roboto Font
    
    Use roboto font using Fout Stager and MuiThemeProvider
    
    Bug #35

---
+++ src/theme.jsx
@@ -0,0 +1,23 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const Roboto300 = { fontFamily: 'Roboto300, sans-serif' };
+const Roboto400 = { fontFamily: 'Roboto400, sans-serif' };
+const Roboto500 = { fontFamily: 'Roboto500, sans-serif' };
+const theme = createMuiTheme({
+  typography: {
+    ...Roboto400,
+    display4: Roboto300,
+    display3: Roboto400,
+    display2: Roboto400,
+    display1: Roboto400,
+    headline: Roboto400,
+    title: Roboto500,
+    subheading: Roboto400,
+    body2: Roboto500,
+    body1: Roboto400,
+    caption: Roboto400,
+    button: Roboto500,
+  },
+});
+
+export default theme;
