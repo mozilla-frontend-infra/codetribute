c0488b4e718d3980f65624dc9fe13c120f489a5c
Fienny Angelina <24544912+fiennyangeln@users.noreply.github.com>
2018-06-23 11:38:21 +0800

    Make link in description inside ProjectCard works (#48)
    * Make link in description inside ProjectCard works
    
    Previously, the link when clicked will direct us to the individual
    project page, instead of the link itself.
    
    Fix #33

--- src/components/ProjectCard/index.jsx
+++ src/components/ProjectCard/index.jsx
@@ -39,6 +39,12 @@ export default class ProjectCard extends Component {
     summary: null,
   };
 
+  handleSummaryClick = event => {
+    if (event.target.href) {
+      event.stopPropagation();
+    }
+  };
+
   render() {
     const {
       classes,
@@ -53,7 +59,9 @@ export default class ProjectCard extends Component {
               {name}
             </Typography>
             {summary && (
-              <Typography className={classes.projectSummary}>
+              <Typography
+                className={classes.projectSummary}
+                onClick={this.handleSummaryClick}>
                 <Markdown source={summary} />
               </Typography>
             )}
