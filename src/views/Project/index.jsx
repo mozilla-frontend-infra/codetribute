8d10a9ab3f28fac77aedd595a84ef869229e196a
Fienny Angelina <fifi.angelina@yahoo.com>
2018-06-27 02:26:17 +0800

    Use the filter icon to display / not display the filter form, Move the filter form to the tasks table from views/project

--- src/views/Project/index.jsx
+++ src/views/Project/index.jsx
@@ -20,7 +20,6 @@ import AppBar from '../../components/AppBar';
 import ErrorPanel from '../../components/ErrorPanel';
 import TasksTable from '../../components/TasksTable';
 import issuesQuery from './issues.graphql';
-import FilterForm from '../../components/FilterForm';
 
 const tagReposMapping = repositories =>
   Object.keys(repositories).reduce((prev, key) => {
@@ -171,7 +170,6 @@ export default class Project extends Component {
               </ExpansionPanelDetails>
             </ExpansionPanel>
           )}
-          <FilterForm />
           {data && data.error && <ErrorPanel error={data.error} />}
           {loading && <Spinner />}
           {!loading && <TasksTable items={issues} />}
