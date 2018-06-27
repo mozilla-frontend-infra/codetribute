d0385e7069a00269d28bcbac159c948e917fe1eb
Fienny Angelina <fifi.angelina@yahoo.com>
2018-06-27 16:09:58 +0800

    Change switch to menu, remove empty div

--- src/components/FilterForm/index.jsx
+++ src/components/FilterForm/index.jsx
@@ -1,56 +1,96 @@
 import { Component } from 'react';
 import { withRouter } from 'react-router-dom';
-import Switch from '@material-ui/core/Switch';
-import FormGroup from '@material-ui/core/FormGroup';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import { parse, stringify } from 'qs';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
 import { withStyles } from '@material-ui/core/styles';
+import { parse, stringify } from 'qs';
+import AccountIcon from 'mdi-react/AccountIcon';
+import { ASSIGNEE } from '../../utils/constants';
+
+const assigneeList = Object.values(ASSIGNEE);
 
 @withRouter
 @withStyles(theme => ({
   root: {
     margin: `0px ${3 * theme.spacing.unit}px`,
+    maxWidth: 360,
   },
 }))
-export default class FilterForm extends Component {
+export default class FilterMenu extends Component {
+  state = {
+    anchorEl: null,
+  };
+
+  getSelectedAssignee() {
+    const { assignee } = this.getQuery();
+
+    return assigneeList.indexOf(assignee) > -1 ? assignee : ASSIGNEE.UNASSIGNED;
+  }
+
   getQuery() {
     const { location } = this.props;
     const query = parse(location.search.slice(1));
 
-    return {
-      ...query,
-      displayAssigned: query.displayAssigned
-        ? query.displayAssigned.toLowerCase() === 'true'
-        : false,
-    };
+    return query;
   }
 
-  handleDisplayAssignedChange = event => {
+  handleClickListItem = event => {
+    this.setState({ anchorEl: event.currentTarget });
+  };
+
+  handleMenuItemClick = (event, assignee) => {
+    this.setState({ anchorEl: null });
     this.props.history.push({
       search: `?${stringify({
         ...this.getQuery(),
-        displayAssigned: event.target.checked,
+        assignee,
       })}`,
     });
   };
 
+  handleClose = () => {
+    this.setState({ anchorEl: null });
+  };
+
   render() {
-    const { displayAssigned } = this.getQuery();
+    const selectedAssignee = this.getSelectedAssignee();
     const { classes } = this.props;
+    const { anchorEl } = this.state;
 
     return (
-      <FormGroup row className={classes.root}>
-        <FormControlLabel
-          control={
-            <Switch
-              color="primary"
-              checked={displayAssigned}
-              onChange={this.handleDisplayAssignedChange}
-            />
-          }
-          label="Also display assigned bugs"
-        />
-      </FormGroup>
+      <div className={classes.root}>
+        <List component="nav">
+          <ListItem
+            button
+            aria-haspopup="true"
+            aria-controls="lock-menu"
+            aria-label="Assignee"
+            onClick={this.handleClickListItem}>
+            <ListItemIcon className={classes.icon}>
+              <AccountIcon />
+            </ListItemIcon>
+            <ListItemText primary="Assignee" secondary={selectedAssignee} />
+          </ListItem>
+        </List>
+        <Menu
+          id="lock-menu"
+          anchorEl={anchorEl}
+          open={Boolean(anchorEl)}
+          onClose={this.handleClose}>
+          {assigneeList.map(assignee => (
+            <MenuItem
+              key={assignee}
+              selected={assignee === selectedAssignee}
+              onClick={event => this.handleMenuItemClick(event, assignee)}>
+              {assignee}
+            </MenuItem>
+          ))}
+        </Menu>
+      </div>
     );
   }
 }
