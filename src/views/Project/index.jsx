03b7bd9d31190e5ae1afe41bc4e460d57f26586e
Fienny Angelina <fifi.angelina@yahoo.com>
2018-07-17 13:39:44 +0700

    Match only language tag

--- src/views/Project/index.jsx
+++ src/views/Project/index.jsx
@@ -60,24 +60,28 @@ const tagReposMapping = repositories =>
 const extractWhiteboardTags = whiteboard => {
   // find occurence like lang=perl,js or lang=perl/js
   const re = /(\w+)=(\w+)[,|/](\w+)/;
-  // find all element within square bracket with minimum string inside
-  const keywords = whiteboard.match(/\[.+?\]/g).reduce((prev, item) => {
-    // remove the square bracket
-    const itemWithoutBracket = item.slice(1, -1);
-    const items = itemWithoutBracket.match(re);
+  // find all element within square bracket starting with lang with as little
+  // string as possible inside
+  const keywords = (whiteboard.match(/\[lang.+?\]/g) || []).reduce(
+    (prev, item) => {
+      // remove the square bracket
+      const itemWithoutBracket = item.slice(1, -1);
+      const items = itemWithoutBracket.match(re);
 
-    // if there is no comma or /
-    if (!items) {
-      return [...prev, itemWithoutBracket];
-    }
+      // if there is no comma or /
+      if (!items) {
+        return [...prev, itemWithoutBracket];
+      }
 
-    // items=["lang=js/xul","lang","js","xul"]
-    // so start looping from 3rd element
-    return [
-      ...prev,
-      ...items.slice(2).map(element => `${items[1]}=${element}`),
-    ];
-  }, []);
+      // items=["lang=js/xul","lang","js","xul"]
+      // so start looping from 3rd element
+      return [
+        ...prev,
+        ...items.slice(2).map(element => `${items[1]}=${element}`),
+      ];
+    },
+    []
+  );
 
   return keywords;
 };
@@ -322,11 +326,9 @@ export default class Project extends Component {
             assignee: bug.status === 'ASSIGNED' ? bug.assignedTo.name : '-',
             project: bug.component,
             tags: [
-              ...new Set([
-                ...(bug.keywords || []),
-                ...((bug.whiteboard && extractWhiteboardTags(bug.whiteboard)) ||
-                  []),
-              ]),
+              ...(bug.keywords || []),
+              ...((bug.whiteboard && extractWhiteboardTags(bug.whiteboard)) ||
+                []),
             ],
             summary: bug.summary,
             lastUpdated: bug.lastChanged,
