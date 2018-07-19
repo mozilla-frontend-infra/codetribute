a2eb43c81f42ebe708d9d58dea9186726326ff0a
Fienny Angelina <fifi.angelina@yahoo.com>
2018-07-19 16:19:10 +0700

    Remove id

--- src/views/Project/index.jsx
+++ src/views/Project/index.jsx
@@ -274,8 +274,7 @@ export default class Project extends Component {
         uniqBy(
           githubData.search.nodes.map(issue => ({
             project: issue.repository.name,
-            id: issue.number,
-            summary: `${issue.number} - ${issue.title}`,
+            summary: issue.title,
             tags: issue.labels.nodes.map(node => node.name).sort(),
             lastUpdated: issue.updatedAt,
             assignee: issue.assignees.nodes[0]
@@ -296,7 +295,7 @@ export default class Project extends Component {
             assignee: bug.status === 'ASSIGNED' ? bug.assignedTo.name : '-',
             project: bug.component,
             tags: bug.keywords || [],
-            summary: `${bug.id} - ${bug.summary}`,
+            summary: bug.summary,
             lastUpdated: bug.lastChanged,
             url: `https://bugzilla.mozilla.org/show_bug.cgi?id=${bug.id}`,
           })),
