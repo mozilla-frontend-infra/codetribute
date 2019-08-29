1d4bf09124224c886ab01470cc247dfabd4fe114
Pankaj Patil <pankaj.patil2099@hotmail.com>
2019-08-29 18:52:52 +0530

    Added links to GSOC and Outreachy (#330)
    * Flex Style
    
    * Added Requested Changes
    
    * Added Rel
    
    * Added Target Attribute

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -15,7 +15,7 @@ import LanguageCsharpIcon from 'mdi-react/LanguageCsharpIcon';
 import LanguageCss3Icon from 'mdi-react/LanguageCss3Icon';
 import LanguageSwiftIcon from 'mdi-react/LanguageSwiftIcon';
 import ConsoleIcon from 'mdi-react/ConsoleIcon';
-import { BUGZILLA_LANGUAGES } from '../../utils/constants';
+import { BUGZILLA_LANGUAGES, THIRD_PARTY_LINKS } from '../../utils/constants';
 import JavaIcon from './JavaIcon';
 import HTMLIcon from './HtmlIcon';
 import PerlIcon from './PerlIcon';
@@ -34,6 +34,18 @@ import RustIcon from './RustIcon';
   text: {
     color: theme.palette.grey[800],
     fontFamily: 'Roboto500',
+  },
+  link: {
+    display: 'inline-flex',
+  },
+  thirdPartyLinks: {
+    padding: 3 * theme.spacing.unit,
+  },
+  sidebarContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    height: '100%',
   },
 }))
 export default class Sidebar extends Component {
@@ -61,25 +73,43 @@ export default class Sidebar extends Component {
 
     // Add Swift directly as there is no `lang=swift` in bugzilla whiteboards
     return (
-      <List disablePadding>
-        {['Swift', ...Object.keys(BUGZILLA_LANGUAGES)].map(language => (
-          <ListItem
-            className={classNames({
-              [classes.active]: language.toLowerCase() === activeLanguage,
-            })}
-            button
-            onClick={this.props.onLanguageClick}
-            id={language}
-            key={language}
-            component={Link}
-            to={`/languages/${language.toLowerCase()}`}>
-            <ListItemIcon>{icons[language] || <WebIcon />}</ListItemIcon>
-            <ListItemText disableTypography className={classes.text}>
-              {language}
-            </ListItemText>
-          </ListItem>
-        ))}
-      </List>
+      <div className={classes.sidebarContainer}>
+        <div>
+          <List disablePadding>
+            {['Swift', ...Object.keys(BUGZILLA_LANGUAGES)].map(language => (
+              <ListItem
+                className={classNames({
+                  [classes.active]: language.toLowerCase() === activeLanguage,
+                })}
+                button
+                onClick={this.props.onLanguageClick}
+                id={language}
+                key={language}
+                component={Link}
+                to={`/languages/${language.toLowerCase()}`}>
+                <ListItemIcon>{icons[language] || <WebIcon />}</ListItemIcon>
+                <ListItemText disableTypography className={classes.text}>
+                  {language}
+                </ListItemText>
+              </ListItem>
+            ))}
+          </List>
+        </div>
+        <div className={classes.thirdPartyLinks}>
+          {THIRD_PARTY_LINKS.map(links => {
+            return (
+              <a
+                key={links.link}
+                rel="noopener noreferrer"
+                target="_blank"
+                className={classes.link}
+                href={links.link}>
+                {links.label}
+              </a>
+            );
+          })}
+        </div>
+      </div>
     );
   }
 }
