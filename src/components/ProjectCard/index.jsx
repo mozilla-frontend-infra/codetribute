fb025b354d674f9319f60a78e8f4ba35ce611cda
Dustin J. Mitchell <dustin@mozilla.com>
2021-02-11 13:58:44 +0000

    Only import the icons we use
    This makes compilation a *lot* faster, and also avoids some warnings
    from mdi-react where various filenames are imported with incorrect case.

--- src/components/ProjectCard/index.jsx
+++ src/components/ProjectCard/index.jsx
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import { string, shape } from 'prop-types';
-import { pascalCase } from 'change-case';
 import { withStyles } from '@material-ui/core/styles';
 import { Link } from 'react-router-dom';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import Markdown from 'react-markdown';
+import ProjectIcon from '../ProjectIcon';
 
 export default
 @withStyles((theme) => ({
@@ -37,51 +37,6 @@ export default
   },
 }))
 class ProjectCard extends Component {
-  state = {
-    projectIcon: null,
-  };
-
-  /* eslint-disable react/no-did-mount-set-state */
-  async componentDidMount() {
-    const { project, classes } = this.props;
-
-    try {
-      if (project.icon) {
-        const mdiName = pascalCase(project.icon);
-        const ProjectIcon = (
-          await import(
-            /* webpackChunkName: "icon" */ `mdi-react/${mdiName}Icon.js`
-          )
-        ).default;
-
-        return this.setState({
-          projectIcon: (
-            <ProjectIcon size={50} className={classes.projectIcon} />
-          ),
-        });
-      }
-
-      const projectIcon = (
-        await import(
-          /* webpackChunkName: "icon" */ `../../images/projectIcons/${project.fileName}.svg`
-        )
-      ).default;
-
-      this.setState({
-        projectIcon: <img height="45" src={projectIcon} alt="Project Icon" />,
-      });
-    } catch (e) {
-      const ProjectIcon = (
-        await import(/* webpackChunkName: "icon" */ `mdi-react/WebIcon.js`)
-      ).default;
-
-      this.setState({
-        projectIcon: <ProjectIcon size={50} className={classes.projectIcon} />,
-      });
-    }
-  }
-  /* eslint-enable react/no-did-mount-set-state */
-
   handleSummaryClick = (event) => {
     if (event.target.href) {
       event.stopPropagation();
@@ -97,15 +52,14 @@ class ProjectCard extends Component {
   render() {
     const {
       classes,
-      project: { name, summary, fileName },
+      project: { icon, name, summary, fileName },
     } = this.props;
-    const { projectIcon } = this.state;
 
     return (
       <Link className={classes.link} to={`projects/${fileName}`}>
         <Card className={classes.card} tabIndex={0}>
           <CardContent className={classes.textAlign}>
-            {projectIcon}
+            <ProjectIcon icon={icon || fileName} />
             <Typography gutterBottom variant="h5" component="h4">
               {name}
             </Typography>
