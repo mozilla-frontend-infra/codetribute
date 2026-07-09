db3bfbcd170c31d1aeef22cd93cc5450d01bffeb
Danny Colin <contact@dannycolin.com>
2026-07-08 20:20:05 -0400

    Fix babel loose mode configuration mismatch

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -409,14 +409,13 @@ module.exports = function (webpackEnv) {
                     },
                   ],
                 ],
-                
+
                 plugins: [
                   // CODETRIBUTE: use decorators
                   [
                     require.resolve('@babel/plugin-proposal-decorators'),
                     { legacy: true },
                   ],
-                  require.resolve('@babel/plugin-proposal-class-properties'),
                   [
                     require.resolve('babel-plugin-named-asset-import'),
                     {
@@ -460,7 +459,7 @@ module.exports = function (webpackEnv) {
                 cacheDirectory: true,
                 // See #6846 for context on why cacheCompression is disabled
                 cacheCompression: false,
-                
+
                 // Babel sourcemaps are needed for debugging into node_modules
                 // code.  Without the options below, debuggers like VSCode
                 // show incorrect code and set breakpoints on the wrong lines.
